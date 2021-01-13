cc25756f43a1c05802baa52a49262b5ca0036ebb
zst111 <shouting_zhang@baofu.com>
2021-01-13 17:26:32 +0800

    已经

---
+++ src/main/java/cases/Test04.java
@@ -0,0 +1,11 @@
+package cases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test04 {
+    @Test
+    public void test01(){
+        Assert.assertEquals("1","1");
+    }
+}
