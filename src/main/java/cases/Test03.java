2e76b9c4197debb22fb42b26a1b7c6893c8bd131
zst111 <shouting_zhang@baofu.com>
2021-01-14 10:43:44 +0800

    已经

--- src/main/java/cases/Test03.java
+++ src/main/java/cases/Test03.java
@@ -1,22 +1,26 @@
 package cases;
 
 import lombok.Data;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Test03 {
 
-    @Test(dataProvider = "dateprovider1")
-    public void test01(String name,int age){
-        System.out.println(name+age);
+    @Test(dataProvider = "dateprovider1",testName = "测试名字")
+    public void test01(String name,int age,String grade){
+
+       // System.out.println(name+age);
+        Assert.assertEquals(10,age);
+
     }
 
     @DataProvider
     public Object[][] dateprovider1(){
         Object[][] o=new Object[][]{
-                {"zhangsan",10},
-                {"lisi",20},
-                {"wangwu",30}
+                {"zhangsan",10,"三班"},
+                {"lisi",20,"四班"},
+                {"wangwu",30,"五班"}
 
 
         };
