32de7a0b4cda8e4a2d4a4f2842706914a343c3fa
zst111 <shouting_zhang@baofu.com>
2021-02-07 17:43:04 +0800

    新增test06

---
+++ src/main/java/cases/Test06.java
@@ -0,0 +1,11 @@
+package cases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test06 {
+    @Test(description = "sdaf")
+    public void test01() {
+        Assert.assertEquals("1", "1");
+    }
+}
